d7db622a8f0f02736e33ef422533580ff6267a75
dugonzal <ciclocontinuo2@gmail.com>
2022-12-25 09:45:31 +0100

    uso la variable mask en vez un uno, para que sea mas claro

--- src/octet/print_bits.c
+++ src/octet/print_bits.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 09:24:24 by ciclo             #+#    #+#             */
-/*   Updated: 2022/12/25 09:35:44 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/12/25 09:44:45 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	print_bits(unsigned char octet)
 	i = 7;
 	while (i >= 0)
 	{
-		byte = (((octet >> i) & 1) + 48);
+		byte = (((octet >> i) & mask) + 48);
 		write (1, &byte, 1);
 		i--;
 	}
