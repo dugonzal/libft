04c8a923b7533b63abd89f3fef052b9c92cc5eff
dugonzal <ciclocontinuo2@gmail.com>
2023-02-21 06:39:57 +0000

    update ft_atoi, max min int

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+         #
+#    By: dugonzal <dugonzal@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/12 01:54:25 by ciclo             #+#    #+#              #
-#    Updated: 2022/12/25 09:43:45 by ciclo            ###   ########.fr        #
+#    Updated: 2023/02/20 02:28:03 by dugonzal         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,20 +21,6 @@ AR			=	ar rcs
 
 
 #Sources
-octet_dir	=	octet/
-octet		=	print_bits reverse_bits swap_bits
-
-math_dir	=	math/
-math		=	is_power_of_2
-
-err_dir		=	err/
-err			=	err
-
-num_dir		= 	ft_num/
-ft_num		=	max min
-
-gnl_dir		=	get_next_line/
-gnl			=	get_next_line get_next_line_utils
 
 FTIS_DIR	=	ft_is/
 FTIS		=	ft_isalnum ft_isalpha ft_isascii ft_isdigit ft_isprint
@@ -43,8 +29,8 @@ FTMEM_DIR	=	ft_mem/
 FTMEM		=	ft_bzero ft_calloc ft_memchr ft_memcmp ft_memmove ft_memset
 
 FTSTR_DIR	=	ft_str/
-FTSTR		=	ft_strlen ft_strchr ft_strdup ft_striteri ft_strjoin ft_strlcat ft_strlen ft_strlcpy \
-				ft_strncmp ft_strnstr ft_strrchr ft_strtrim ft_substr ft_split arr_size ft_strpbrk
+FTSTR		=	ft_strlen ft_strchr ft_strdup ft_striteri ft_strjoin ft_strlcat ft_strlen ft_strlcpy ft_strncmp ft_strnstr ft_strrchr ft_strtrim ft_substr ft_split
+
 FTPUT_DIR	=	ft_put/
 FTPUT		=	ft_putchar_fd ft_putendl_fd ft_putnbr_fd
 
@@ -55,62 +41,52 @@ FTLST_DIR	=	ft_lst/
 FTLST		=	ft_lstadd_back ft_lstadd_front ft_lstclear ft_lstdelone \
 				ft_lstiter ft_lstlast ft_lstmap ft_lstnew ft_lstsize
 
-SRC_FILES	+=	$(addprefix $(FTIS_DIR),$(FTIS))
-SRC_FILES	+=	$(addprefix $(FTMEM_DIR),$(FTMEM))
-SRC_FILES	+=	$(addprefix $(FTPUT_DIR),$(FTPUT))
-SRC_FILES	+=	$(addprefix $(FTTO_DIR),$(FTTO))
-SRC_FILES	+=	$(addprefix $(FTSTR_DIR),$(FTSTR))
-SRC_FILES	+=	$(addprefix $(FTLST_DIR),$(FTLST))
-SRC_FILES	+=	$(addprefix $(gnl_dir),$(gnl))
-SRC_FILES	+=	$(addprefix $(err_dir),$(err))
-SRC_FILES	+=	$(addprefix $(math_dir),$(math))
-SRC_FILES	+=	$(addprefix $(num_dir),$(ft_num))
-SRC_FILES	+=	$(addprefix $(octet_dir),$(octet))
-
+SRC_FILES+=$(addprefix $(FTIS_DIR),$(FTIS))
+SRC_FILES+=$(addprefix $(FTMEM_DIR),$(FTMEM))
+SRC_FILES+=$(addprefix $(FTPUT_DIR),$(FTPUT))
+SRC_FILES+=$(addprefix $(FTTO_DIR),$(FTTO))
+SRC_FILES+=$(addprefix $(FTSTR_DIR),$(FTSTR))
+SRC_FILES+=$(addprefix $(FTLST_DIR),$(FTLST))
 
 SRCS 		= 	$(addprefix $(SRCS_DIR), $(addsuffix .c, $(SRC_FILES)))
 OBJS 		= 	$(addprefix $(OBJS_DIR), $(addsuffix .o, $(SRC_FILES)))
 
+
 OBJSF		=	.cache_exists
 
 # Rules for the main program and the objects files
-$(NAME):	$(OBJS)
-			@make -C ft_printf
-			@mv ft_printf/printf.a $(NAME)
-			@$(AR) $(NAME) $(OBJS)
-			@echo "$(GREEN)Libft compiled!$(DEF_COLOR)"
+$(NAME): $(OBJS)
+	@make -C ft_printf
+	@mv ft_printf/printf.a $(NAME)
+	@$(AR) $(NAME) $(OBJS)
+	@echo "$(GREEN)Libft compiled!$(DEF_COLOR)"
 
 $(OBJS_DIR)%.o : $(SRCS_DIR)%.c | $(OBJSF)# | $(INCLUDES) directory if you want to use header files
-			@$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
+	@$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
 
 $(OBJSF):#  create the directory for the objects if it doesn't exist mkdir -p para que no de error si ya existe
-			@mkdir	-p $(OBJS_DIR)
-			@mkdir	-p  $(OBJS_DIR)$(FTIS_DIR)
-			@mkdir	-p $(OBJS_DIR)$(FTMEM_DIR)
-			@mkdir	-p $(OBJS_DIR)$(FTPUT_DIR)
-			@mkdir	-p $(OBJS_DIR)$(FTTO_DIR)
-			@mkdir	-p $(OBJS_DIR)$(FTSTR_DIR)
-			@mkdir  -p $(OBJS_DIR)$(FTLST_DIR)
-			@mkdir	-p $(OBJS_DIR)$(gnl_dir)
-			@mkdir	-p $(OBJS_DIR)$(err_dir)
-			@mkdir	-p $(OBJS_DIR)$(math_dir)
-			@mkdir	-p $(OBJS_DIR)$(num_dir)
-			@mkdir	-p $(OBJS_DIR)$(octet_dir)
+	@mkdir	-p $(OBJS_DIR)
+	@mkdir	-p  $(OBJS_DIR)$(FTIS_DIR)
+	@mkdir	-p $(OBJS_DIR)$(FTMEM_DIR)
+	@mkdir	-p $(OBJS_DIR)$(FTPUT_DIR)
+	@mkdir	-p $(OBJS_DIR)$(FTTO_DIR)
+	@mkdir	-p $(OBJS_DIR)$(FTSTR_DIR)
+	@mkdir  -p $(OBJS_DIR)$(FTLST_DIR)
 
 all:		$(NAME)
 
 clean:
-			$(RM)	$(OBJS_DIR)
-			make clean -C ft_printf
-			$(RM)	$(OBJSF)
-			echo "Libft objects deleted!"
+	@$(RM)	$(OBJS_DIR)
+	@make clean -C ft_printf
+	@$(RM)	$(OBJSF)
+	@echo "Libft objects deleted!"
 
 fclean:		clean
-			make fclean -C ft_printf
-			$(RM)	$(NAME)
-			@echo "libft cleaned!"
+	@make fclean -C ft_printf
+	@$(RM)	$(NAME)
+	@echo "libft cleaned!"
 
 re:			fclean all
-			@echo "libft recompiled!"
+	@echo "libft recompiled!"
 
 .PHONY:		all clean fclean re
--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 17:59:05 by ciclo             #+#    #+#             */
-/*   Updated: 2023/02/17 12:54:48 by ciclo            ###   ########.fr       */
+/*   Updated: 2023/02/21 06:37:51 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,91 +18,71 @@
 # include <string.h>
 # include <stdio.h>
 # include "../ft_printf/include/ft_printf.h"
-# include "../src/get_next_line/include/get_next_line.h"
 # define GREEN	"\033[0;32m"
 # define RED 	"\033[1;31m"
 # define GREY 	"\033[0;90m"
 # define CYAN	"\033[1;96m"
 # define RESET 	"\033[0m"
+
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }					t_list;
 
-int				ft_isalnum(int c);
-int				ft_isalpha(int c);
-int				ft_isascii(int c);
-int				ft_isdigit(int c);
-int				ft_isprint(int c);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 // ft_lst
 
-t_list			*ft_lstnew(void *content);
-void			ft_lstadd_front(t_list **lst, t_list *new);
-int				ft_lstsize(t_list *lst);
-t_list			*ft_lstlast(t_list *lst);
-void			ft_lstadd_back(t_list **lst, t_list *new);
-void			ft_lstdelone(t_list *lst, void (*del)(void *));
-void			ft_lstclear(t_list **lst, void (*del)(void *));
-void			ft_lstiter(t_list *lst, void (*f)(void *));
-t_list			*ft_lstmap(t_list *lst, void \
-*(*f)(void *), void (*del)(void *));
-size_t			arr_size(char **str);
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 // ft_str
 
-int				ft_count_words(char const *str, char c);
-size_t			ft_strlen(const char *s);
-char			**ft_split(char const *s, char c);
-char			*ft_strchr(const char *s, int c);
-char			*ft_strdup(const char *s1);
-void			ft_striteri(char *s, void (*f)(unsigned int, char *));
-char			*ft_strjoin(char const *s1, char const *s2);
-size_t			ft_strlcat(char *dst, const char *src, size_t dstsize);
-size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
-char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-int				ft_strncmp(const char *s1, const char *s2, size_t n);
-char			*ft_strnstr(const char *haystack, \
-const char *needle, size_t len);
-size_t			ft_strcspn(const char *s, const char *reject);
-char			*ft_strpbrk(const char *s1, const char *s2);
-
+int		ft_count_words(char const *str, char c);
+size_t	ft_strlen(const char *s);
+char	**ft_split(char const *s, char c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 // ft_mem
 
-void			ft_bzero(void *s, size_t n);
-void			*ft_calloc(size_t count, size_t size);
-void			*ft_memchr(const void *s, int c, size_t n);
-int				ft_memcmp(const void *s1, const void *s2, size_t n);
-void			*ft_memcpy(void *dst, const void *src, size_t n);
-void			*ft_memmove(void *dst, const void *src, size_t len);
-void			*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
 // ft_put
 
-void			ft_putchar_fd(char c, int fd);
-void			ft_putendl_fd(char *s, int fd);
-void			ft_putnbr_fd(int n, int fd);
-void			ft_putstr_fd(char *s, int fd);
-char			*ft_strrchr(const char *s, int c);
-char			*ft_strtrim(char const *s1, char const *set);
-char			*ft_substr(char const *s, unsigned int start, size_t len);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 // ft_tol
 
-int				ft_atoi(const char *str);
-char			*ft_itoa(int n);
-int				ft_tolower(int c);
-int				ft_toupper(int c);
-
-// errr
-void			err(char *str);
-// math
-
-int				is_power_of_2(unsigned int n);
-// ft_num
-int				max(int *tab, int len);
-int				min(int *tab, int len);
-
-// bytes
-void			print_bits(unsigned char octet);
-unsigned char	swap_bits(unsigned char octet);
-unsigned char	reverse_bits(unsigned char octet);
+int		ft_atoi(const char *str);
+char	*ft_itoa(int n);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 #endif
