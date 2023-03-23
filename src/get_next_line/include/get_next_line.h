a4d4d2ca171a8e282719498c8643afb927d15540
Duvan Gonzalez <dugonzal@c3r1s6.42urduliz.com>
2023-03-23 15:27:16 +0100

    header gnl

--- src/get_next_line/include/get_next_line.h
+++ src/get_next_line/include/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dugonzal <dugonzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 14:59:17 by dugonzal          #+#    #+#             */
-/*   Updated: 2023/03/23 15:16:18 by dugonzal         ###   ########.fr       */
+/*   Updated: 2023/03/23 15:26:58 by dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@
 char	*ft_strjoin2(char *s1, char *s2);
 int		find(char *str, int c);
 int		ft_strlen2(char *str);
+char	*get_next_line(int fd);
 
 #endif
