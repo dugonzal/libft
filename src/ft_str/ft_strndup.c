f59b5e4e7820810ae2901b2320bb75be9030ba04
dugonzal <ciclcontinuo2@gmail.com>
2023-04-27 14:21:25 +0000

    actualizo comprobacion de parametros

--- src/ft_str/ft_strndup.c
+++ src/ft_str/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 14:06:57 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/04/27 14:18:49 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/04/27 14:21:00 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ char	*ft_strndup(const char *str, size_t n)
 {
 	char *tmp;
 
-	if (!str || !n)
-	  return (NULL);
+	if (!n)
+	  return ((char *)str);
+	else if (!str)
+		return (NULL);
 	tmp = malloc(sizeof(char) * (n + 1));
 	if (!tmp)
 		return (NULL);
