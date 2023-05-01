cdb5bd74052ee4c658e73301052c40c28eb535db
dugonzal <ciclcontinuo2@gmail.com>
2023-05-01 19:47:41 +0000

    agrego : a la funcion de erro basica

--- src/err/err.c
+++ src/err/err.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 18:10:26 by ciclo             #+#    #+#             */
-/*   Updated: 2023/02/17 12:54:58 by ciclo            ###   ########.fr       */
+/*   Updated: 2023/05/01 19:47:18 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@
 /// @param str parametro de  mensaje presonalizado
 void	err(char *str)
 {
-	printf (RED"Error %s\n"RESET, str);
+	printf (RED"Error: %s\n"RESET, str);
 	exit (1);
 }
